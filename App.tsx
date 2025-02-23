19e3b724f97f7ef8ef849d1819d3e52242df28c2
CarlosSTS <carlossts826@gmail.com>
2025-02-23 18:06:47 -0300

    feat: Create example

--- App.tsx
+++ App.tsx
@@ -1,130 +1,188 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {
-  ScrollView,
-  StatusBar,
+  Alert,
+  FlatList,
+  Image,
+  Modal,
   StyleSheet,
   Text,
-  useColorScheme,
+  TouchableOpacity,
   View,
 } from 'react-native';
+import {RTNUtils} from '@carlossts/rtn-utils';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+interface MapApp {
+  name: string;
+  package: string;
+  icon?: string;
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+type MapApps = MapApp[] | undefined;
+const App: React.FC = () => {
+  const [mapApps, setMapApps] = useState<MapApps>([]);
+  const [modalVisible, setModalVisible] = useState(false);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const getLocationApps = useCallback(async () => {
+    try {
+      const apps = await RTNUtils?.getLocationApps({
+        includesBase64: true,
+      });
+      setMapApps(apps);
+    } catch (error) {
+      const errorMessage =
+        error instanceof Error ? error.message : 'An unknown error occurred';
+      Alert.alert('openGlobalSettings Failed', errorMessage);
+    }
+  }, []);
 
-  /*
-   * To keep the template simple and small we're adding padding to prevent view
-   * from rendering under the System UI.
-   * For bigger apps the reccomendation is to use `react-native-safe-area-context`:
-   * https://github.com/AppAndFlow/react-native-safe-area-context
-   *
-   * You can read more about it here:
-   * https://github.com/react-native-community/discussions-and-proposals/discussions/827
-   */
-  const safePadding = '5%';
+  useEffect(() => {
+    getLocationApps();
+  }, [getLocationApps]);
+
+  const onRequestModal = useCallback(() => {
+    setModalVisible(state => !state);
+  }, []);
+
+  const handleAppOpen = useCallback(async (app: MapApp) => {
+    let scheme = '';
+    const lat = -4.128489;
+    const lng = -38.2593854;
+    const label = 'My Location Test';
+    if (app.package === 'com.waze') {
+      scheme = `waze://?ll=${lat},${lng}&navigate=yes`;
+    } else {
+       scheme = `geo:0,0?q=${lat},${lng}(${label})`;
+    }
+
+    try {
+      await RTNUtils?.openAppWithLocation({
+        packageName: app.package,
+        url: scheme,
+      });
+    } catch (error) {
+      Alert.alert(
+        'openAppWithLocation Failed',
+        error instanceof Error ? error.message : 'An unknown error occurred',
+      );
+    }
+  }, []);
+
+  const renderAppItem = useCallback(({item}: {item: MapApp}) => {
+    return (
+      <TouchableOpacity
+        activeOpacity={0.7}
+        style={styles.appItem}
+        onPress={() => handleAppOpen(item)}>
+        <Image source={{uri: item.icon}} style={styles.appIcon} />
+        <Text style={styles.appName}>{item.name}</Text>
+      </TouchableOpacity>
+    );
+  }, [handleAppOpen]);
 
   return (
-    <View style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        style={backgroundStyle}>
-        <View style={{paddingRight: safePadding}}>
-          <Header/>
+    <View style={styles.container}>
+      <TouchableOpacity
+        activeOpacity={0.9}
+        style={styles.openButton}
+        onPress={onRequestModal}>
+        <Text style={styles.openButtonText}>Open modal with apps</Text>
+      </TouchableOpacity>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={onRequestModal}>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalTitle}>Open with ({mapApps?.length})</Text>
+            <FlatList
+              horizontal={true}
+              showsHorizontalScrollIndicator={false}
+              data={mapApps}
+              renderItem={renderAppItem}
+              keyExtractor={item => item.package}
+              contentContainerStyle={styles.appList}
+            />
+
+            <TouchableOpacity
+              activeOpacity={0.9}
+              style={styles.cancelButton}
+              onPress={onRequestModal}>
+              <Text style={styles.cancelButtonText}>Cancelar</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            paddingHorizontal: safePadding,
-            paddingBottom: safePadding,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+      </Modal>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5F5F9',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  openButton: {
+    backgroundColor: '#007BFF',
+    padding: 15,
+    borderRadius: 10,
   },
-  sectionDescription: {
-    marginTop: 8,
+  openButtonText: {
+    color: '#FFF',
+    fontSize: 16,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+  },
+  modalContent: {
+    width: '90%',
+    backgroundColor: '#F5F5F9',
+    borderRadius: 20,
+    padding: 20,
+    marginBottom: 20,
+  },
+  modalTitle: {
     fontSize: 18,
-    fontWeight: '400',
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
   },
-  highlight: {
-    fontWeight: '700',
+  appList: {
+    justifyContent: 'space-between',
+    flexGrow: 1,
+  },
+  appItem: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: '#F0F0F0',
+  },
+  appIcon: {
+    width: 40,
+    height: 40,
+    borderRadius: 10,
+  },
+  appName: {
+    fontSize: 16,
+    color: '#000',
+  },
+  cancelButton: {
+    width: '100%',
+    backgroundColor: '#666',
+    padding: 15,
+    borderRadius: 20,
+    marginTop: 20,
+    alignItems: 'center',
+  },
+  cancelButtonText: {
+    color: '#FAFAFA',
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
 
